5a400472b5e28ebfe7fa5a547ba18d6ce50d2748
Sam Gammon <sam@elide.ventures>
2024-07-07 21:32:11 -0700

    feat: use `mimalloc`
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use miette::Result;
 use orogene::Orogene;
 
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 #[async_std::main]
 async fn main() -> Result<()> {
     Ok(Orogene::load().await?)
