5d27aa1736ede4abaa3f19683579a5549d72269c
Sam Gammon <sam@elide.ventures>
2024-07-07 22:26:35 -0700

    chore: more allocators, simd-json
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use miette::Result;
 use orogene::Orogene;
 
+#[cfg(feature = "alloc-mimalloc")]
 #[global_allocator]
-static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+#[cfg(feature = "alloc-snmalloc")]
+#[global_allocator]
+static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
+
+#[cfg(feature = "alloc-snmalloc")]
+#[global_allocator]
+static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
 #[async_std::main]
 async fn main() -> Result<()> {
